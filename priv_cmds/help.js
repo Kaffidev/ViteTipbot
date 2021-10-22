3da26529c78942b7949c0e5a0e9d0be6dc03a76d
Nihat <kaffinpx@gmail.com>
2021-10-23 00:28:04 +0300

    Update some messages

--- priv_cmds/help.js
+++ priv_cmds/help.js
@@ -3,7 +3,7 @@ module.exports = {
   execute (client, event, env) {
     client.v1.sendDm({
       recipient_id: env.senderId,
-      text: 'Commands:\nhelp, deposit, withdraw, balance(bal), tip, statistics(stats)\nReply(tweet) Commands: tip (amount) <token>\n\nHelpful links:\nhttps://vite.org/ - Vites Official Website\nhttps://twitter.com/KaffinP/status/1441859291468779520 - How to use commands'
+      text: 'Commands:\nhelp, deposit, withdraw, balance(bal), tip, statistics(stats)\nReply(tweet) Commands: tip (amount) <token>\n\nHelpful links:\nhttps://vite.org/ - Vites Official Website'
     })
   }
 }
