cf056ec660bffc73c1f5e64c94222ca805a46b3c
Nihat <kaffinpx@gmail.com>
2021-11-09 22:37:12 +0300

    Update

--- priv_cmds/help.js
+++ priv_cmds/help.js
@@ -4,13 +4,15 @@ module.exports = {
     client.v1.sendDm({
       recipient_id: env.senderId,
       text: `Commands:\n\n
-help : Shows commands\n
-deposit : Gives deposit wallet address and memo\n
-withdraw (rec.) (amo.) <token> : Withdraws assets to recipient\n 
-balance(bal) <token> : Shows balance\n
-tip (@receipt) (amount) <token> : Tip users semi-privately\n
-statistics(stats) <token> : Shows stats\n
-\n\nReply(tweet) Commands:\ntip (amount) <token>\n\nA command example: ?tip @TipbotVite 1 VITE`
+?help : Shows commands
+?deposit : Gives deposit wallet address and memo
+?withdraw (rec.) (amo.) <token> : Withdraws assets to recipient
+?balance(bal) <token> : Shows balance
+?tip (@receipt) (amount) <token> : Tip users semi-privately
+?statistics(stats) <token> : Shows stats
+\nReply(tweet) Commands:
+?tip (amount) <token>
+\nExample: ?tip @TipbotVite 1 VITE`
     })
   }
 }
