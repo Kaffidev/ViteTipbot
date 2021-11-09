c4fd7a48081c59c1003f83eb38707078b6e2add0
Nihat <kaffinpx@gmail.com>
2021-11-09 20:53:07 +0300

    Update messages

--- priv_cmds/help.js
+++ priv_cmds/help.js
@@ -3,14 +3,14 @@ module.exports = {
   execute (client, event, env) {
     client.v1.sendDm({
       recipient_id: env.senderId,
-      text: `Commands:\n
+      text: `Commands:\n\n
 help : Shows commands\n
 deposit : Gives deposit wallet address and memo\n
 withdraw (rec.) (amo.) <token> : Withdraws assets to recipient\n 
 balance(bal) <token> : Shows balance\n
 tip (@receipt) (amount) <token> : Tip users semi-privately\n
 statistics(stats) <token> : Shows stats\n
-\n\nReply(tweet) Commands:\ntip (amount) <token>\n\nFor more info execute command without args.`
+\n\nReply(tweet) Commands:\ntip (amount) <token>\n\nA command example: ?tip @TipbotVite 1 VITE`
     })
   }
 }
